f8d58e2fce3bd3f46f1273b28d5e6446d435f286
kataboy00 <kataboy00@gmail.com>
2025-11-21 11:31:44 -0500

    remove roster var

--- roster.py
+++ roster.py
@@ -1,7 +1,7 @@
 # https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
-roster =["Evans", "High", "Brown"]
-player = {"Last Name": roster,
+
+player = {"Last Name": ["Evans", "High", "Brown"],
           "First Name": ["Kyan", "Zayden", "James"],
           "height": [74, 82, 82],
           "weight": [175, 230, 240]}
